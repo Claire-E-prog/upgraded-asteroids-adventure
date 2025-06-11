0e982bcf97b30716b194945de864c091cfb7956a
Claire-E-Prog <clairesander525@gmail.com>
2025-06-11 07:57:13 -0500

    working game loop. Player class has functional rotate and move methods to move the player

--- main.py
+++ main.py
@@ -2,23 +2,32 @@
 # the open-source pygame library
 # throughout this file
 
-import pygame
+import pygame 
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    x = SCREEN_WIDTH/2
+    y = SCREEN_WIDTH/2
+    player = Player(x, y)
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(type(screen))
-    i = 1
-    while i < 10:
+    dt = 0
+    game_over = False
+    while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000)
+        screen.fill((000000))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
